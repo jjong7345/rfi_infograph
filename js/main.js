a0c2a41aaa51717a22e6e07990be0827e1c5bb05
Jong Suk Park <jjong7345@gmail.com>
2013-11-27 17:23:36 -0500

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//create RFINFO namspace if not already defined
+var RFINFO = RFINFO || {};
+
+(function ($) {
+  RFINFO.Site = (function() {
+    
+
+
+      
+
+  }());
+
+}(jQuery.noConflict()));
